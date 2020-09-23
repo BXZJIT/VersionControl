4b9cadcb984ed9cd172d8e0bda40a078febbdfb4
BXZJIT <peter.bezzeg@stud.uni-corvinus.hu>
2020-09-23 14:56:35 +0200

    Writing into CSV file

--- VersionControl/VersionControl/Form1.cs
+++ VersionControl/VersionControl/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +20,7 @@ namespace VersionControl
             InitializeComponent();
             label1.Text = VersionControlResource.LastName; 
             button1.Text = VersionControlResource.Add;
+            button2.Text = VersionControlResource.Write;
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
             listBox1.DisplayMember = "FullName";
@@ -29,6 +31,30 @@ namespace VersionControl
             var u = new User();
             u.FullName = textBox1.Text;
             users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+           
+            sfd.InitialDirectory = Application.StartupPath;
+            sfd.Filter = "Comma Seperated Values (*.csv)|*.csv";
+            sfd.DefaultExt = "csv";
+            sfd.AddExtension = true;
+
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+              
+                foreach (var s in users)
+                {
+                    sw.Write(s.ID);
+                    sw.Write(";");
+                    sw.Write(s.FullName);
+                    sw.Write(";");
+                    sw.WriteLine();
+                }
+            }
         }
     }
 }
