c57402dbf3eb9c34defad57d65504c881b73d25a
BXZJIT <peter.bezzeg@stud.uni-corvinus.hu>
2020-09-23 14:40:51 +0200

    Adding listboxes

--- VersionControl/VersionControl/Form1.cs
+++ VersionControl/VersionControl/Form1.cs
@@ -7,17 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VersionControl.Entities;
 
 namespace VersionControl
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
             label1.Text = VersionControlResource.LastName; 
             label2.Text = VersionControlResource.FirstName; 
-            button1.Text = VersionControlResource.Add; 
+            button1.Text = VersionControlResource.Add;
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = textBox1.Text,
+                FirstName = textBox2.Text
+            };
+            users.Add(u);
         }
     }
 }
