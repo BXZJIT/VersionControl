00fddd3283a135cefee4f4320ba3b2610d5d210a
BXZJIT <peter.bezzeg@stud.uni-corvinus.hu>
2020-10-06 12:39:26 +0200

    storing portfolio in memory

--- BXZJIT_5.GYAK/BXZJIT_5.GYAK/Form1.cs
+++ BXZJIT_5.GYAK/BXZJIT_5.GYAK/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BXZJIT_5.GYAK.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,13 +13,24 @@ namespace BXZJIT_5.GYAK
 {
     public partial class Form1 : Form
     {
+       
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
+        List<Portfolioitem> Portfolio = new List<Portfolioitem>();
         public Form1()
         {
             InitializeComponent();
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
+            CreatePortfolio();
+        }
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new Portfolioitem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new Portfolioitem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new Portfolioitem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
