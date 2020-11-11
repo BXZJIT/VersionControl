cee8c313258fd1c872a9eb2494e69b16b3457575
BXZJIT <peter.bezzeg@stud.uni-corvinus.hu>
2020-11-11 13:50:07 +0100

    CarFactory

--- GYAK8_BXZJIT/GYAK8_BXZJIT/Entities/CarFactory.cs
+++ GYAK8_BXZJIT/GYAK8_BXZJIT/Entities/CarFactory.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace GYAK8_BXZJIT.Entities
 {
-    public class BallFactory : IToyFactory
+    public class CarFactory: IToyFactory
     {
         public Toy CreateNew()
         {
