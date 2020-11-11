744abfa53a779ab7b57385f14a23f35e4628d5f3
BXZJIT <peter.bezzeg@stud.uni-corvinus.hu>
2020-11-11 14:07:46 +0100

    Buttons

--- GYAK8_BXZJIT/GYAK8_BXZJIT/Form1.Designer.cs
+++ GYAK8_BXZJIT/GYAK8_BXZJIT/Form1.Designer.cs
@@ -32,6 +32,9 @@
             this.mainPanel = new System.Windows.Forms.Panel();
             this.createTimer = new System.Windows.Forms.Timer(this.components);
             this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
+            this.Car = new System.Windows.Forms.Button();
+            this.ball = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // mainPanel
@@ -53,15 +56,48 @@
             this.conveyorTimer.Interval = 10;
             this.conveyorTimer.Tick += new System.EventHandler(this.conveyorTimer_Tick);
             // 
+            // Car
+            // 
+            this.Car.Location = new System.Drawing.Point(36, 23);
+            this.Car.Name = "Car";
+            this.Car.Size = new System.Drawing.Size(75, 23);
+            this.Car.TabIndex = 1;
+            this.Car.Text = "Car";
+            this.Car.UseVisualStyleBackColor = true;
+            this.Car.Click += new System.EventHandler(this.Car_Click);
+            // 
+            // ball
+            // 
+            this.ball.Location = new System.Drawing.Point(154, 23);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(75, 23);
+            this.ball.TabIndex = 2;
+            this.ball.Text = "Ball";
+            this.ball.UseVisualStyleBackColor = true;
+            this.ball.Click += new System.EventHandler(this.ball_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(276, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Coming next";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.Car);
             this.Controls.Add(this.mainPanel);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,6 +106,9 @@
         private System.Windows.Forms.Panel mainPanel;
         private System.Windows.Forms.Timer createTimer;
         private System.Windows.Forms.Timer conveyorTimer;
+        private System.Windows.Forms.Button Car;
+        private System.Windows.Forms.Button ball;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- GYAK8_BXZJIT/GYAK8_BXZJIT/Form1.cs
+++ GYAK8_BXZJIT/GYAK8_BXZJIT/Form1.cs
@@ -14,12 +14,16 @@ namespace GYAK8_BXZJIT
 {
     public partial class Form1 : Form
     {
+        private Toy _nextToy;
         private List<Toy> _toys = new List<Toy>();
         private IToyFactory _factory;
         public IToyFactory Factory
         {
             get { return _factory; }
-            set { _factory = value; }
+            set { 
+                _factory = value;
+                DisplayNext();
+            }
         }
 
         public Form1()
@@ -52,6 +56,25 @@ namespace GYAK8_BXZJIT
                 mainPanel.Controls.Remove(oldesToy);
                 _toys.Remove(oldesToy);
             }
+        }
+
+        private void Car_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void ball_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory();
+        }
+        private void DisplayNext()
+        {
+            if (_nextToy != null)
+                Controls.Remove(_nextToy);
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = label1.Top + label1.Height + 20;
+            _nextToy.Left = label1.Left;
+            Controls.Add(_nextToy);
         }
     }
 }
